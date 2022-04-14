626e09bc39faa9bac10f4ebf39651b6df6d0c757
Kulo0901-haru <101855314+Kulo0901-haru@users.noreply.github.com>
2022-04-14 11:13:39 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#open').click(function() {
+    $('.overlay').fadeIn();
+    $('.ura').fadeIn('');
+  });
+  $('#close').click(function() {
+    $('.overlay').fadeOut();
+    $('.ura').fadeOut();
+ });
+
+  $('#open2').click(function() {
+   $('.about').fadeIn();
+   });
